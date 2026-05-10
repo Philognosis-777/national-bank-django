6d94fcaf852e8ef0a56af9ad4c6320e6de63f3d7
Yared Girum <yaredmallpana@gmail.com>
2026-05-10 21:37:24 +0300

    hi

--- national_bank/national_bank/settings.py
+++ national_bank/national_bank/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ec!e&6rumhdyh@)kiklp=gohoo5dx^uwsrhr)-rttg0=g0mf2f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'national_bank.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = ['https://national_bank.onrender.com']
 # Application definition
